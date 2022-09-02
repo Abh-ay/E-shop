e4d0e170ecf719a1d1fe6bd86dd47bbfd4ad3979
Abh-ay <abhayyy1912@gmail.com>
2022-09-02 16:35:05 +0530

    remove "/"

--- e_shopper/settings.py
+++ e_shopper/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["j-k-textiles.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["j-k-textiles.herokuapp.com", "localhost"]
 
 
 # Application definition

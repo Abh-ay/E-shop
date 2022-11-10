2950645c4251a4ca2acaf8527d17c61c955067fe
Abh-ay <abhayyy1912@gmail.com>
2022-11-10 18:43:36 +0530

    remove staticfiledirs

--- e_shopper/settings.py
+++ e_shopper/settings.py
@@ -172,7 +172,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'

cede118712af1acf6184258d87849f227a6f9627
Abh-ay <abhayyy1912@gmail.com>
2022-11-10 18:37:49 +0530

    ran staticfile cmd

--- e_shopper/settings.py
+++ e_shopper/settings.py
@@ -99,18 +99,18 @@ WSGI_APPLICATION = 'e_shopper.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #   'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': config('PG_DATABASE'),
-    #     'USER': config('PG_USER'),
-    #     'PASSWORD': config('PG_PASSWORD'),
-    #     'HOST': config('PG_HOST'),
-    #     'PORT': config('PG_PORT'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('PG_DATABASE'),
+        'USER': config('PG_USER'),
+        'PASSWORD': config('PG_PASSWORD'),
+        'HOST': config('PG_HOST'),
+        'PORT': config('PG_PORT'),
+    }
 }
 
 
